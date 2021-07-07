31449fd979474d4416093b119b47b5519abe29d5
kinyeramos2021 <kinyeramo@gmail.com>
2021-07-07 16:04:08 +0300

    final copy

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\ContactController;
+use App\Http\Controllers\InformationController;
 
 Route::get('/', [App\Http\Controllers\ContactController::class, 'contactForm'])->name('contact-form');
 Route::post('/contact-form', [App\Http\Controllers\ContactController::class, 'storeContactForm'])->name('contact-form.store');
